09613caa6df24e7e6e50df09d8e6a4aeb5529bc7
Arda Unsalan <211307077@kocaeli.edu.tr>
2024-12-06 22:02:20 +0300

    refactor: s3 parameters

--- Course_Django_Project/settings.py
+++ Course_Django_Project/settings.py
@@ -151,12 +151,16 @@ else:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_BUCKET_PARAMETER = {
+    AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 3600,
         'CacheControl': 'max-age=86400',
     }
 
-    STATIC_URL=f'https://{AWS_STORAGES_BUCKET_NAME}.s3.amazonaws.com/static/'
+    STATICFILES_LOCATION = 'static'
+    AWS_LOCATION = 'static'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGES_BUCKET_NAME}.s3.amazonaws.com/'
+
+    STATIC_URL=f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     STATIC_ROOT = STATIC_URL
 
     MEDIA_LOCATION = 'media'
